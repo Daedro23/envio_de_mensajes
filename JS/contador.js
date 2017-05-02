3d38c3a40e14bbe15e9827591701d4fdc2420000
Daedro23 <daedro23@gmail.com>
2017-05-02 03:49:31 -0400

    Campo numerico

--- JS/contador.js
+++ JS/contador.js
@@ -11,3 +11,14 @@ function contar() {
              } 
     } 
 
+function valida(e){
+    tecla = (document.all) ? e.keyCode : e.which;
+
+    if (tecla==8){
+        return true;
+    }
+        
+    patron =/[0-9]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+}
